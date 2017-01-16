2e1af10d7c49056b3979e9d7fd9c24d9a24b206f
magicalcritical <magicalcritical@gmail.com>
2017-01-16 14:53:25 +0000

    fixing compass

--- tree/urls.py
+++ tree/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns  = [
 
 url(r'^dir/', views.direction, name='direction'),
-url(r'^', views.show_genres, name='genre'),
+url(r'^$', views.show_genres, name='genre'),
 url(r'^index/$', views.index, name='index'),
 
 
